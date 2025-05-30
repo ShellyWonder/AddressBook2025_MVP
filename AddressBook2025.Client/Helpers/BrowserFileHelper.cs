e75d417d36046134ec4f5b1e41cf479636c55b60
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-05-29 20:12:18 -0500

    Finish Contact form UI and enabled image upload and display; swapped out profile image placeholder; created contactform.razor.css and BrowseFileHelper.cs

---
+++ AddressBook2025.Client/Helpers/BrowserFileHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace AddressBook2025.Client.Helpers
+{
+    public class BrowserFileHelper
+    {
+        public static readonly string DefaultContactImage ="/img/ProfilePlaceHolder.png";
+        public static int MaxFileSize = 5 * 1024 * 1024; // 5 MB
+
+        public static async Task<string> GetDataUrl(IBrowserFile file)
+        { 
+            using Stream filestream = file.OpenReadStream(MaxFileSize);
+            using MemoryStream ms = new();
+            await filestream.CopyToAsync(ms);
+
+            byte[] imageBytes = ms.ToArray();
+            // Convert the byte array to a Base64 string(displayed as the image)
+            string imageBase64 = Convert.ToBase64String(imageBytes);
+            string dataUrl = $"data:{file.ContentType};base64,{imageBase64}";
+            return dataUrl;
+
+        }
+        
+    }
+}
--- AddressBook2025/Services/CategoryDTOService.cs
+++ AddressBook2025/Services/CategoryDTOService.cs
@@ -7,8 +7,6 @@ namespace AddressBook2025.Services
 {
     public class CategoryDTOService(ICategoryRepository repository) : ICategoryDTOService
     {
-
-
         public async Task<CategoryDTO> CreateCategoryAsync(CategoryDTO category, string userId)
         {
             Category newCategory = new()
@@ -19,8 +17,6 @@ namespace AddressBook2025.Services
         newCategory = await repository.CreateCategoryAsync(newCategory);
             return newCategory.ToDTO();
         }
-
-       
 
         // List of entities and transform them to DTOs
         public async Task<List<CategoryDTO>> GetCategoriesAsync(string userId)
