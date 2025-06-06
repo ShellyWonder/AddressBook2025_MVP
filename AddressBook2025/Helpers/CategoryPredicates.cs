54ac300b8df1ddb0ed2933af22801258dce1fc96
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-06-05 23:50:20 -0500

    added helper methods

---
+++ AddressBook2025/Helpers/CategoryPredicates.cs
@@ -0,0 +1,12 @@
+﻿using AddressBook2025.Models;
+using System.Linq.Expressions;
+
+namespace AddressBook2025.Helpers
+{
+    public static class CategoryPredicates
+    {
+        public static Expression<Func<Category, bool>> ByCategoryIdAndUser(int categoryId, string userId) =>
+                                          c => c.Id == categoryId && c.AppUserId == userId;
+
+    }
+}
---
+++ AddressBook2025/Helpers/ContactPredicates.cs
@@ -0,0 +1,18 @@
+﻿using AddressBook2025.Models;
+using System.Linq.Expressions;
+
+namespace AddressBook2025.Helpers
+{
+    public static class ContactPredicates
+    {
+        //Matches the single contact that belongs to a logged in user
+        public static Expression<Func<Contact, bool>> ByIdAndUser(int id, string userId) =>
+            c=> c.Id == id && c.AppUserId == userId;
+
+        public static Expression<Func<Contact, bool>> ByContactIdAndContactUser(Contact contact) =>
+            c => c.Id == contact.Id && c.AppUserId == contact.AppUserId;
+        public static Expression<Func<Contact, bool>> ByContactIdAndUser(int contactId, string userId) =>
+            c => c.Id == contactId && c.AppUserId == userId;
+
+    }
+}
