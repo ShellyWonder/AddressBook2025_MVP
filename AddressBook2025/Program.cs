7cf1be3e0afe5e65d23d32f98d8dba87601f3b1c
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-06-20 20:50:19 -0500

    Configuring the db connection for deployment to Railway

--- AddressBook2025/Program.cs
+++ AddressBook2025/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DbConnection")
+var connectionString = DataUtility.GetConnectionString(builder.Configuration)
                                      ?? throw new InvalidOperationException("Connection string 'DbConnection' not found.");
 
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
