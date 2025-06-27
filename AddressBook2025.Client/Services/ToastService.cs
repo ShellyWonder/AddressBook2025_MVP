dc46fe31664bf5b8858e907add6e3c0359b5feca
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-06-27 09:59:18 -0500

    Created and registered ToastService in order to make it globally available to deeper nested functionality.

---
+++ AddressBook2025.Client/Services/ToastService.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBook2025.Client.Services
+{
+    public class ToastService
+    {
+        public event Action<string>? OnShow;
+
+        public void Show(string message)
+        {
+            OnShow?.Invoke(message);
+        }
+    }
+}
