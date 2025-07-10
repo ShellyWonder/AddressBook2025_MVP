4733fd0a647f9da93d02f69913e0e491f5833f33
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-07-09 23:32:29 -0500

    Enhanced search functionality by creating ContactSearchModel, adding a model and validationto the input element.

---
+++ AddressBook2025.Client/Models/ContactSearchModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook2025.Client.Models
+{
+    public class ContactSearchModel
+    {
+        [Required(ErrorMessage = "Please enter a name to search.")]
+        [MinLength(2, ErrorMessage = "Search term must be at least 2 characters.")]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z\s'-]+$", ErrorMessage = "Please use letters, spaces, apostrophes, or hyphens only.")]
+
+        public string? SearchTerm { get; set; }
+
+    }
+}
