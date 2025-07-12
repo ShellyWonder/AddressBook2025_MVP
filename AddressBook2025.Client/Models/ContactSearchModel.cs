76923e99f9a980b86e8191ec392ab98fd805b369
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-07-11 22:38:33 -0500

    refactored search components

--- AddressBook2025.Client/Models/ContactSearchModel.cs
+++ AddressBook2025.Client/Models/ContactSearchModel.cs
@@ -7,8 +7,7 @@ namespace AddressBook2025.Client.Models
         [Required(ErrorMessage = "Please enter a name to search.")]
         [MinLength(2, ErrorMessage = "Search term must be at least 2 characters.")]
         [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
-        [RegularExpression(@"^[a-zA-Z\s'-]+$", ErrorMessage = "Please use letters, spaces, apostrophes, or hyphens only.")]
-
+       
         public string? SearchTerm { get; set; }
 
     }
